c52a687c6cf1827818387e1287aac5ce3983b99e
wrajer <wrajer@users.noreply.github.com>
2019-03-13 15:11:16 +0100

    v2

--- src/main/java/gwd/quiz2/controller/TestControllers.java
+++ src/main/java/gwd/quiz2/controller/TestControllers.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Scope(value = "session")
@@ -16,26 +17,53 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestControllers {
 
 
-    private User user; //dla zmiennych klasy jest rowne 0, nie
+    private User user = new User(); //dla zmiennych klasy jest rowne 0, nie
     // widzimy construktora bo jest ona w fabry i tworzy czesto zmienna statyczną, czego nie chcemy
 
     @Autowired
     private TestRepository testRepository;
 
+    //dla testu nr pytania
+    private Integer questionNumber;
+
     //@ResponseBody //gdy nie chce się robić html
+
+
     @GetMapping("/tests/{id}/play")
-    public String play (@PathVariable Integer id, ModelMap modelMap) {
+    public String play(@PathVariable Integer id, ModelMap modelMap) {
+        questionNumber = 0;
         Test test = testRepository.findById(id).get(); //wyjęcie z bazy danych obiektu pytania
-
         modelMap.addAttribute("test", test);
-        modelMap.addAttribute("question", test.getQuestionList().get(0));
-
-//        user.addPoint();
-//        return "" + user.getNumberOfPoints();
-
+        modelMap.addAttribute("question", test.getQuestionList().get(questionNumber));
+//ja w swojej miałem jescze ++
+      //  user = new User();
+      //  questionNumber=0;
         return "test";
-
     }
+
+
+//controller michala
+@GetMapping("/tests/{id}/continue")
+public String continueTest(
+        @RequestParam(required = false)
+                boolean correct, @PathVariable Integer id, ModelMap modelMap) {
+    Test test = testRepository.findById(id).get();
+    if ( test.getQuestionList().get(questionNumber).getCorrect() == correct){
+        user.addPoint();
+    }
+    questionNumber++;
+    if ( questionNumber < test.getQuestionList().size() ){
+        modelMap.put("test",test);
+        modelMap.put("question",test.getQuestionList().get(questionNumber));
+    }
+    else{
+        modelMap.put("test",test);
+        modelMap.put("message","Koniec gry! Twoja liczba punktów to: "
+                +user.getNumberOfPoints());
+    }
+    return "test";
+}
+
 
 }
 
